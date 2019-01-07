3013c5c7c8fad938624f5863d3ae448d2d2f1c08
Munkhtegsh <munkhtegsh@outlook.com>
2019-01-06 17:59:36 -0800

    Sync controller with the server

---
+++ server/controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+  get_restaurants: (req, res) => {
+    console.log('Hi');
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const ctr = require('./controller.js');
 const app = express();
 
 // ########################################
 //            Middleware
 // ########################################
 app.use(bodyParser.json());
+app.get('/api/restaurants', ctr.get_restaurants);
 
 const PORT = 4000;
-app.listen(PORT => console.log(`App is listening on ${PORT}`));
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
